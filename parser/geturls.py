3ebd18e24a2516b34d4f723fa2faef8dc3781753
toph <toph.allen@gmail.com>
2013-02-25 17:08:27 -0500

    Minor changes, nothing substantial.

--- parser/geturls.py
+++ parser/geturls.py
@@ -1,5 +1,8 @@
 import csv
-from HTMLParser import HTMLParser
+import lxml
+from bs4 import BeautifulSoup
+import os
 
 # Create a subclass of HTMLParser 
-class outbreakHTMLParser(HTMLParser):
+class outbreak_html_parser(HTMLParser):
+    def 
