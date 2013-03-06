439ee1cb889086363c8a80f79261064933871e9c
toph <toph.allen@gmail.com>
2013-03-06 12:17:23 -0500

    Downloader.py works in an informal non-GIDEON test.

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -1,17 +1,31 @@
-import sys
 import os
 import csv
+import gidcommon as gc
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-# from selenium import selenium
+
+os.chdir(gc.datadir)
+urls = open(gc.outdir + 'gideon-dl-urls-test.csv')
+
+urlreader = csv.reader(urls)
+urlreader.next()
+
+email = ''
+password = ''
 
 driver = webdriver.Firefox()
-driver.get("http://www.gideononline.com")
-driver.find_element_by_name("email").send_keys("toph.allen@yahoo.com")
-driver.find_element_by_name("password").send_keys("pwgideon", Keys.RETURN)
+# driver.get("http://www.gideononline.com")
+# driver.find_element_by_name("email").send_keys(email)
+# driver.find_element_by_name("password").send_keys(password, Keys.RETURN)
 
-# sel = selenium('localhost', 4444, '*firefox', 'http://www.google.com')
-# sel.start()
-# sel.open("http://www.gideononline.com")
-# sel.type("email","toph.allen@yahoo.com")
-# sel.type("password","pwgideon")
+for row in urlreader:
+    pathogen = row[0]
+    country = row[1]
+    url = row[2]
+    driver.get(url)
+    f = open(gc.indir + "outbreaks/" + pathogen + "_" + country + ".html", 'w+')
+    src = driver.page_source
+    f.write(src.encode('utf8'))
+    f.close()
+
+driver.close()
